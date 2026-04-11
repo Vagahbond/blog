f241c00ab63b5380f26b2e0d0e66e32c7219072b
Vagahbond <vagahbond@pm.me>
2026-04-11 21:20:00 +1200

    frontend:reconfiure nginx

--- nix/module.nix
+++ nix/module.nix
@@ -92,11 +92,11 @@
                   ];
                 };
 
-                extraConfig = ''
-                  proxy_http_version 1.1;
-                  proxy_set_header Upgrade $http_upgrade;
-                  proxy_set_header Connection "upgrade";
-                '';
+                # extraConfig = ''
+                #   proxy_http_version 1.1;
+                #   proxy_set_header Upgrade $http_upgrade;
+                #   proxy_set_header Connection "upgrade";
+                # '';
               };
         };
       })
