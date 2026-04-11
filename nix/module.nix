954a813144134e2cdffaf6891a97777615922581
Vagahbond <vagahbond@pm.me>
2026-04-11 21:23:41 +1200

    frontend:reconfiure nginx

--- nix/module.nix
+++ nix/module.nix
@@ -85,18 +85,18 @@
                 locations."/" = {
                   proxyWebsockets = true; # needed if you need to use WebSocket
                   proxyPass = "http://127.0.0.1:${toString config.services.touches-grasses.grassServer.port}";
-                  proxyHttpVersion = "1.1"; # Required for WebSockets
-                  proxySetHeader = [
-                    "Upgrade $http_upgrade" # Required for WebSocket upgrade
-                    "Connection \"upgrade\"" # Required for WebSocket upgrade
-                  ];
+                  # proxyHttpVersion = "1.1"; # Required for WebSockets
+                  # proxySetHeader = [
+                  #   "Upgrade $http_upgrade" # Required for WebSocket upgrade
+                  #   "Connection \"upgrade\"" # Required for WebSocket upgrade
+                  # ];
                 };
 
-                # extraConfig = ''
-                #   proxy_http_version 1.1;
-                #   proxy_set_header Upgrade $http_upgrade;
-                #   proxy_set_header Connection "upgrade";
-                # '';
+                extraConfig = ''
+                  proxy_http_version 1.1;
+                  proxy_set_header Upgrade $http_upgrade;
+                  proxy_set_header Connection "upgrade";
+                '';
               };
         };
       })
